2ae40bb1742e93ed29361243b3e09f33a4363cac
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2019-10-08 05:15:11 -0300

    resolver IP externa

---
+++ ExtIP/ExtIP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+
+
+/* Intenta establecer la IP externa de la terminal
+ * 
+ */
+
+namespace ExtIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ips;
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    ips = client.DownloadString("http://canihazip.com/s");
+                }
+                catch (WebException e)
+                {
+                    ips = "Sitio fuera de linea?";
+                }
+
+                Console.WriteLine("Mi ip es -> {0}", ips);
+            }
+
+            
+
+        }
+
+        
+    }
+}
