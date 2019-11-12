4012bfd88dd0977b03cd02dd861c98405042c9df
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2019-11-12 06:05:02 -0300

    cuadros de dialogo y un async rudimentario

---
+++ WinAsyncSample/WinAsyncSample/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WinAsyncSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_generar = new System.Windows.Forms.Button();
+            this.txt_box = new System.Windows.Forms.TextBox();
+            this.lbl_count = new System.Windows.Forms.Label();
+            this.btn_Abrir = new System.Windows.Forms.Button();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.btn_Procesa = new System.Windows.Forms.Button();
+            this.conversorTexto = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_generar
+            // 
+            this.btn_generar.Location = new System.Drawing.Point(27, 35);
+            this.btn_generar.Name = "btn_generar";
+            this.btn_generar.Size = new System.Drawing.Size(125, 23);
+            this.btn_generar.TabIndex = 0;
+            this.btn_generar.Text = "Generar Texto";
+            this.btn_generar.UseVisualStyleBackColor = true;
+            this.btn_generar.Click += new System.EventHandler(this.btn_generar_Click);
+            // 
+            // txt_box
+            // 
+            this.txt_box.Location = new System.Drawing.Point(27, 77);
+            this.txt_box.Multiline = true;
+            this.txt_box.Name = "txt_box";
+            this.txt_box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_box.Size = new System.Drawing.Size(387, 232);
+            this.txt_box.TabIndex = 1;
+            // 
+            // lbl_count
+            // 
+            this.lbl_count.AutoSize = true;
+            this.lbl_count.Location = new System.Drawing.Point(31, 332);
+            this.lbl_count.Name = "lbl_count";
+            this.lbl_count.Size = new System.Drawing.Size(121, 13);
+            this.lbl_count.TabIndex = 2;
+            this.lbl_count.Text = "Cantidad de Caracteres:";
+            // 
+            // btn_Abrir
+            // 
+            this.btn_Abrir.Location = new System.Drawing.Point(158, 35);
+            this.btn_Abrir.Name = "btn_Abrir";
+            this.btn_Abrir.Size = new System.Drawing.Size(125, 23);
+            this.btn_Abrir.TabIndex = 3;
+            this.btn_Abrir.Text = "Abrir Archivo";
+            this.btn_Abrir.UseVisualStyleBackColor = true;
+            this.btn_Abrir.Click += new System.EventHandler(this.btn_Abrir_Click);
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.Location = new System.Drawing.Point(289, 35);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(125, 23);
+            this.btn_Guardar.TabIndex = 4;
+            this.btn_Guardar.Text = "Guardar Archivo";
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
+            // 
+            // btn_Procesa
+            // 
+            this.btn_Procesa.Location = new System.Drawing.Point(27, 358);
+            this.btn_Procesa.Name = "btn_Procesa";
+            this.btn_Procesa.Size = new System.Drawing.Size(387, 51);
+            this.btn_Procesa.TabIndex = 5;
+            this.btn_Procesa.Text = "Procesar";
+            this.btn_Procesa.UseVisualStyleBackColor = true;
+            this.btn_Procesa.Click += new System.EventHandler(this.btn_Procesa_Click);
+            // 
+            // conversorTexto
+            // 
+            this.conversorTexto.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackGroundWorker1_ComenzarTarea);
+            this.conversorTexto.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackGroundWorker1_finalizado);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 421);
+            this.Controls.Add(this.btn_Procesa);
+            this.Controls.Add(this.btn_Guardar);
+            this.Controls.Add(this.btn_Abrir);
+            this.Controls.Add(this.lbl_count);
+            this.Controls.Add(this.txt_box);
+            this.Controls.Add(this.btn_generar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ejemplo Async";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_generar;
+        private System.Windows.Forms.TextBox txt_box;
+        private System.Windows.Forms.Label lbl_count;
+        private System.Windows.Forms.Button btn_Abrir;
+        private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.Button btn_Procesa;
+        private System.ComponentModel.BackgroundWorker conversorTexto;
+    }
+}
+
---
+++ WinAsyncSample/WinAsyncSample/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Ejemplo del uso de faker para generar texto, el paquete nuguet se llama Bogus.
+// Tambien como implementar los cuadros de dialogo de abrir y guardar archivo
+//por ultimo un pequeño ejemplo de como usar subprocesos al
+//invocar un worker que trunca el contenido del control de texto..
+
+
+namespace WinAsyncSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarBackGroundWorker();
+        }
+
+        private void InicializarBackGroundWorker()
+        {
+            conversorTexto.DoWork += new DoWorkEventHandler(BackGroundWorker1_ComenzarTarea);
+            conversorTexto.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackGroundWorker1_finalizado);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "Seleccione un Archivo de Texto",
+                Filter = "Archivo Texto (*.txt)|*.txt",
+                Title = "Abrir un Archivo de Texto"
+            };
+
+        }
+
+        private void BackGroundWorker1_finalizado(object sender, RunWorkerCompletedEventArgs e)
+        {
+            txt_box.Text = e.Result.ToString();
+            lbl_count.Text = $"Cantidad de Caracteres: {txt_box.TextLength.ToString()}";
+            txt_box.Refresh();
+            btn_Abrir.Enabled = true;
+            btn_generar.Enabled = true;
+            btn_Guardar.Enabled = true;
+            btn_Procesa.Enabled = true;
+        }
+
+        private void BackGroundWorker1_ComenzarTarea(object sender, DoWorkEventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            btn_Abrir.Enabled = false;
+            btn_generar.Enabled = false;
+            btn_Guardar.Enabled = false;
+            btn_Procesa.Enabled = false;
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+            e.Result = TransformarTexto((TextBox) e.Argument, worker, e);
+        }
+
+        private string GenerarParrafo()
+        {
+            var faker = new Faker("es_MX");
+            string texto = faker.Lorem.Paragraph(15);
+            return texto;
+
+        }
+
+        private string TransformarTexto(TextBox box, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            lbl_count.Text = "Procesando...";
+            char[] contenedor = box.Text.ToCharArray();
+            string reverso = string.Empty;
+
+            for (int i = contenedor.Length -1 ; i > -1; i--)
+            {
+                reverso += contenedor[i];
+            }
+           
+            Thread.Sleep(5000); //Para probar los workers
+            return reverso;
+
+        }
+
+
+        private void btn_generar_Click(object sender, EventArgs e)
+        {
+
+            btn_generar.Text = "Agregar Más";
+            txt_box.Text += GenerarParrafo();
+            lbl_count.Text = $"Cantidad de Caracteres: {txt_box.TextLength.ToString()}";
+            txt_box.Refresh();
+
+        }
+
+
+        private void btn_Abrir_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    txt_box.Text = sr.ReadToEnd();
+                    lbl_count.Text = $"Cantidad de Caracteres: {txt_box.TextLength.ToString()}";
+                    sr.Close();
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Error de Seguridad.\n\n Mensaje: {ex.Message}\n\n" +
+                    $"Detalles:\n\n{ex.StackTrace}");
+                }
+
+                
+            }
+        }
+
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogoGuardar = new SaveFileDialog();
+            dialogoGuardar.Filter = "Archivo Texto (*.txt)|*.txt";
+            dialogoGuardar.Title = "Guardar Archivo Texto";
+            dialogoGuardar.ShowDialog();
+
+            if (dialogoGuardar.FileName != "")
+            {
+
+                if (!File.Exists(dialogoGuardar.FileName)) // Si el archivo No existe
+                {
+                    File.Create(dialogoGuardar.FileName).Close(); // Crear Archivo
+                    using (StreamWriter sw = File.AppendText(dialogoGuardar.FileName))
+                    {
+                        if(txt_box.TextLength > 0)
+                            sw.WriteLine(txt_box.Text); // Escribir el contenido del textbox si no  esta vacio
+                        else
+                            sw.WriteLine(""); // generar un archivo vacio
+                    }
+                }
+                else // si Ya existe el archivo
+                {
+                    
+                    using (StreamWriter sw = File.AppendText(dialogoGuardar.FileName))
+                    {
+                        if (txt_box.TextLength > 0)
+                            sw.WriteLine(txt_box.Text); // escribir el contenido del box
+                        else
+                            sw.WriteLine(""); // generar un archivo vacio
+                    }
+
+                }
+            }
+
+            dialogoGuardar.Dispose();
+
+        }
+
+        private void btn_Procesa_Click(object sender, EventArgs e)
+        {
+            conversorTexto.RunWorkerAsync(txt_box);
+        }
+    }
+}
