998603c7d666176512e2dfe6c98ffc9475506b40
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2019-12-10 05:30:45 -0300

    servicios en NetFramework

---
+++ ServicioConsola/ServicioConsola/Latencia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ServicioConsola
+{
+    public class Latencia
+    {
+        private readonly Timer _timer;
+
+        public Latencia()
+        {
+            _timer = new Timer(5000) { AutoReset = true };
+            _timer.Elapsed += timer_Disparo;
+        }
+
+        private void timer_Disparo(object sender, ElapsedEventArgs e)
+        {
+            string[] lineas = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Temp\Demo\Latencia.txt", lineas);
+        }
+
+        public void Iniciar()
+        {
+            _timer.Start();
+        }
+
+        public void Detener()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ ServicioConsola/ServicioConsola/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+
+/*
+ * Pequeño ejemplo de servicio y consola
+ * para no lidiar con el tema de registrar el servicio uso TopShelf
+ * 
+ * para registrar el servicio hay que ubicar el programa en un directorio
+ * con privilegios e inamovible
+ * 
+ * luego desde la consola se puede ejecutar 
+ * programa.exe install start
+ * 
+ * 
+ */
+
+namespace ServicioConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+           {
+               x.Service<Latencia>(s => 
+               {
+                   s.ConstructUsing(latencia => new Latencia());
+                   s.WhenStarted(latencia => latencia.Iniciar());
+                   s.WhenStopped(latencia => latencia.Detener());
+               });
+
+               x.RunAsLocalSystem();
+               x.SetServiceName("ServicioLatencia");
+               x.SetDisplayName("Servicio de Latencia");
+               x.SetDescription("Ejemplo de servicio corriendo con TopShelf");
+
+           });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+}
