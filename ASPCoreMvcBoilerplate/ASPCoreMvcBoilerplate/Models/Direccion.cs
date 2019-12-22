5ebec3d2f5091d3d3a57c7b91f6bcabf10b94549
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2019-12-22 05:46:13 -0300

    snipets de vistas para confeccion rapida

---
+++ ASPCoreMvcBoilerplate/ASPCoreMvcBoilerplate/Models/Direccion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreMvcBoilerplate.Models
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+        public int Depto { get; set; }
+        public string Localidad { get; set; }
+        public string Provincia { get; set; }
+
+
+    }
+}
